dfd12c50986f8f12acbb4af6c83dc74c9b3987b1
Lev Lazinskiy <lev@circleci.com>
2015-11-22 14:14:54 -0800

    Automatically Create DB

--- app/__init__.py
+++ app/__init__.py
@@ -31,4 +31,8 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
+
     return app
