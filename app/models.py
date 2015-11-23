723870b4f1f6a39817abe3dc11cc88438ca93980
Lev Lazinskiy <lev@circleci.com>
2015-11-22 18:48:52 -0800

    Store and Render Markdown in DB

--- app/models.py
+++ app/models.py
@@ -5,6 +5,9 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 from flask import current_app
 from datetime import datetime
+from markdown import markdown
+
+import bleach
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -64,5 +67,14 @@ class Note(db.Model):
     __tablename__ = 'notes'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(markdown(value, output_format='html'),
+        tags=allowed_tags, strip=True))
+
+db.event.listen(Note.body, 'set', Note.on_changed_body)
