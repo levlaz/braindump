b7302607debf96ed40a1de48e91a9ddec6655940
Lev Lazinskiy <lev@circleci.com>
2015-11-23 13:25:14 -0800

    Only Show Notes that belong to the logged in user

--- app/main/views.py
+++ app/main/views.py
@@ -9,11 +9,14 @@ from ..models import User, Note
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NoteForm()
-    if form.validate_on_submit():
-        note = Note(title=form.title.data,body=form.body.data,
-            author=current_user._get_current_object())
-        db.session.add(note)
-        return redirect(url_for('.index'))
-    notes = Note.query.order_by(Note.timestamp.desc()).all()
-    return render_template('index.html', form=form, notes=notes)
+    if current_user.is_authenticated():
+        form = NoteForm()
+        if form.validate_on_submit():
+            note = Note(title=form.title.data,body=form.body.data,
+                author=current_user._get_current_object())
+            db.session.add(note)
+            return redirect(url_for('.index'))
+        notes = Note.query.filter_by(author_id=current_user.id).order_by(Note.timestamp.desc()).all()
+        return render_template('index.html', form=form, notes=notes)
+    else:
+        return render_template('index.html')
