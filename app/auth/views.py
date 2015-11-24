ac620a83c609a9d1d151a33e0066d3c06ac3634a
Lev Lazinskiy <lev@circleci.com>
2015-11-24 13:48:47 -0800

    Force Email Case to be lower
    Fixes https://github.com/levlaz/braindump/issues/4

--- app/auth/views.py
+++ app/auth/views.py
@@ -19,7 +19,7 @@ def before_request():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
+        user = User.query.filter_by(email=form.email.data.lower()).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
@@ -37,7 +37,7 @@ def logout():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email=form.email.data,
+        user = User(email=form.email.data.lower(),
             username=form.username.data,
             password=form.password.data)
         db.session.add(user)
