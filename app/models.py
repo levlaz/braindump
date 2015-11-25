963702de420f0312edd8cd937885014a29c39e5c
Lev Lazinskiy <lev@circleci.com>
2015-11-25 13:39:53 -0800

    [SECURITY] Only Show Secure Gravatar

--- app/models.py
+++ app/models.py
@@ -112,10 +112,7 @@ class User(UserMixin, db.Model):
         return True
 
     def gravatar(self, size=100, default='identicon', rating='g'):
-        if request.is_secure:
-            url = 'https://secure.gravatar.com/avatar'
-        else:
-            url = 'http://www.gravatar.com/avatar'
+        url = 'https://secure.gravatar.com/avatar'
         hash = self.avatar_hash or hashlib.md5(
             self.email.encode('utf-8')).hexdigest()
         return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
