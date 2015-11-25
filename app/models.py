1161126759b745f03de7d0149120ec97d7c1e013
Lev Lazinskiy <lev@circleci.com>
2015-11-25 08:26:46 -0800

    [SECURITY] - Updated hash to SHA512

--- app/models.py
+++ app/models.py
@@ -41,7 +41,7 @@ class User(UserMixin, db.Model):
 
     @password.setter
     def password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password,method='pbkdf2:sha512')
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
--- manage.py
+++ manage.py
@@ -24,8 +24,7 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Follow=Follow, Role=Role,
-                Permission=Permission, Post=Post, Comment=Comment)
+    return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
