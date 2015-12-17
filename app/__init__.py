a1046bcf696de80ab1a4ac6132f5003c89afaaaa
acemaster <vivekhtc25@gmail.com>
2015-12-17 12:25:38 +0300

    resolves levlaz/braindump #97 Added github oauth login

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask.ext.mail import Mail
 from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+from flask_oauthlib.client import OAuth
 
 from config import config
 
@@ -14,6 +15,18 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+oauth = OAuth()
+github = oauth.remote_app(
+        'github',
+        consumer_key='e4dab2c621b44a75bd51',
+        consumer_secret='bde0d04cd12e0c8e887eba324af849356dd16d10',
+        request_token_params={'scope': 'user:email'},
+        base_url='https://api.github.com/',
+        request_token_url=None,
+        access_token_method='POST',
+        access_token_url='https://github.com/login/oauth/access_token',
+        authorize_url='https://github.com/login/oauth/authorize'
+        )
 
 
 def create_app(config_name):
@@ -26,6 +39,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    oauth.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
