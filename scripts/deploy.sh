2d03797c26739865090e5684d0598f68e5ea676f
Lev Lazinskiy <lev@circleci.com>
2016-05-27 20:49:45 -0700

    Set up Nginx Container [ci skip]

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 
 # Send Latest Scripts to Production Server
 rsync -avz scripts/ circleci@159.203.236.126:/var/www/braindump/scripts/
+rsync -avz etc/ circleci@159.203.236.126:/var/www/braindump/etc/
 scp docker-compose.yml circleci@159.203.236.126:/var/www/braindump
 
 # Log into Production Server, Pull and Restart Docker
