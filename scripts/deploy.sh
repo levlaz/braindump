cd03935cdbbdb2a7281c47edf1021b51f9462079
Lev Lazinskiy <lev@levlaz.org>
2016-05-27 22:29:32 -0700

    Update Nginx Configuration and use TLS

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,4 +7,5 @@ scp docker-compose.yml circleci@159.203.236.126:/var/www/braindump
 
 # Log into Production Server, Pull and Restart Docker
 ssh circleci@159.203.236.126 'cd /var/www/braindump && docker-compose pull'
+ssh circleci@159.203.236.126 'cd /var/www/braindump && docker-compose build'
 ssh circleci@159.203.236.126 'cd /var/www/braindump && source scripts/secrets.sh && docker-compose up -d'
