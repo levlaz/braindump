47ffafc0f1cc2985f7f82b776835772e85b0d862
Lev Lazinskiy <lev@levlaz.org>
2016-05-29 23:38:38 -0700

    Remove Cron from startup script

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -8,7 +8,5 @@ until bash -c "python manage.py db current"; do
 done
 
 >&2 echo "Postgres is up, starting braindump"
-# Update Crontab
-bash -c "for file in etc/cron/*; do crontab $file; done"
 # Run Migrations and Start App
 bash -c "python manage.py deploy && gunicorn manage:app -b 0.0.0.0"
