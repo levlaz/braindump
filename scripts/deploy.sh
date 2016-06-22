7cf4754bec8ad68c9bc809e7c2f4c4bc8c434c25
Lev Lazinskiy <lev@levlaz.org>
2016-06-21 21:35:27 -0400

    Update Host and Docker

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Send Latest Scripts to Production Server
-rsync -avz scripts/ circleci@159.203.236.126:/var/www/braindump/scripts/
-rsync -avz etc/ circleci@159.203.236.126:/var/www/braindump/etc/
-scp docker-compose.yml circleci@159.203.236.126:/var/www/braindump
+rsync -avz scripts/ circleci@app.levops.net:/var/www/braindump/scripts/
+rsync -avz etc/ circleci@app.levops.net:/var/www/braindump/etc/
+scp docker-compose.yml circleci@app.levops.net:/var/www/braindump
 
 # Log into Production Server, Pull and Restart Docker
-ssh circleci@159.203.236.126 'cd /var/www/braindump && docker-compose pull'
-ssh circleci@159.203.236.126 'cd /var/www/braindump && docker-compose build'
-ssh circleci@159.203.236.126 'cd /var/www/braindump && source scripts/secrets.sh && docker-compose up -d'
+ssh circleci@app.levops.net 'cd /var/www/braindump && docker-compose pull'
+ssh circleci@app.levops.net 'cd /var/www/braindump && docker-compose build'
+ssh circleci@app.levops.net 'cd /var/www/braindump && source scripts/secrets.sh && docker-compose up -d'
