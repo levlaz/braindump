1b1d29055b9be448b840ae1b3159607083a8ba0c
Frederick-S <mao_xiaodan@hotmail.com>
2016-07-02 05:33:14 +0800

    current_user.is_anonymous is called as a method (#265)

--- app/auth/views.py
+++ app/auth/views.py
@@ -90,7 +90,7 @@ def confirm(token):
 
 @auth.route('/unconfirmed')
 def unconfirmed():
-    if current_user.is_anonymous() or current_user.confirmed:
+    if current_user.is_anonymous or current_user.confirmed:
         return redirect(url_for('main.index'))
     return render_template('auth/unconfirmed.html')
 
