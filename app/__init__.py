0457d9f45f07512fcc9fe21739758fb8dfb75cd3
Lev Lazinskiy <lev@circleci.com>
2016-08-21 13:22:46 -0700

    login modal

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_wtf.csrf import CsrfProtect
 from config import config
 
 bootstrap = Bootstrap()
@@ -11,6 +12,7 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+csrf = CsrfProtect()
 
 
 def create_app(config_name):
@@ -21,6 +23,7 @@ def create_app(config_name):
     mail.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    csrf.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
