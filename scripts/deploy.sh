120c433afac8c2cf53d7d4ff17285bbd35fb7420
Lev Lazinskiy <lev@circleci.com>
2016-08-27 22:53:48 -0700

    Remove call to moment
    fix https://github.com/levlaz/braindump/issues/266

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Send Latest Scripts to Production Server
-rsync -avz scripts/ circleci@app.levops.net:/var/www/braindump/scripts/
-rsync -avz etc/ circleci@app.levops.net:/var/www/braindump/etc/
-scp docker-compose.yml circleci@app.levops.net:/var/www/braindump
+rsync -avz scripts/ root@app.levops.net:/var/www/braindump/scripts/
+rsync -avz etc/ root@app.levops.net:/var/www/braindump/etc/
+scp docker-compose.yml root@app.levops.net:/var/www/braindump
 
 # Log into Production Server, Pull and Restart Docker
-ssh circleci@app.levops.net 'cd /var/www/braindump && docker-compose pull'
-ssh circleci@app.levops.net 'cd /var/www/braindump && docker-compose build'
-ssh circleci@app.levops.net 'cd /var/www/braindump && source scripts/secrets.sh && docker-compose up -d'
+ssh root@app.levops.net 'cd /var/www/braindump && docker-compose pull'
+ssh root@app.levops.net 'cd /var/www/braindump && docker-compose build'
+ssh root@app.levops.net 'cd /var/www/braindump && source scripts/secrets.sh && docker-compose up -d'
