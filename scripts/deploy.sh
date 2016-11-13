72e037a8f73755b1295bc8770a7f29bbbcdf10bf
Lev Lazinskiy <lev@levlaz.org>
2016-11-12 17:31:21 -0800

    Update all Deployment Scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Send Latest Scripts to Production Server
-rsync -avz scripts/ root@app.levops.net:/var/www/braindump/scripts/
-rsync -avz etc/ root@app.levops.net:/var/www/braindump/etc/
-scp docker-compose.yml root@app.levops.net:/var/www/braindump
+rsync -avz scripts/ circle@hydrogen.levlaz.org:/var/www/braindump/scripts/
+rsync -avz etc/ circle@hydrogen.levlaz.org:/var/www/braindump/etc/
+scp docker-compose.yml circle@hydrogen.levlaz.org:/var/www/braindump
 
 # Log into Production Server, Pull and Restart Docker
-ssh root@app.levops.net 'cd /var/www/braindump && docker-compose pull'
-ssh root@app.levops.net 'cd /var/www/braindump && docker-compose build'
-ssh root@app.levops.net 'cd /var/www/braindump && source scripts/secrets.sh && docker-compose up -d'
+ssh circle@hydrogen.levlaz.org 'cd /var/www/braindump && docker-compose pull'
+ssh circle@hydrogen.levlaz.org 'cd /var/www/braindump && docker-compose build'
+ssh circle@hydrogen.levlaz.org 'cd /var/www/braindump && source scripts/secrets.sh && docker-compose up -d'
