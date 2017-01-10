1750cd21f19a1a37e7d5b9336aa55f4bbcb83895
Lev Lazinskiy <lev@levlaz.org>
2017-01-09 18:57:25 -0800

    Switch to ENVAR for production server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Send Latest Scripts to Production Server
-rsync -avz scripts/ circle@hydrogen.levlaz.org:/var/www/braindump/scripts/
-rsync -avz etc/ circle@hydrogen.levlaz.org:/var/www/braindump/etc/
-scp docker-compose.yml circle@hydrogen.levlaz.org:/var/www/braindump
+rsync -avz scripts/ $PROD_SERVER:/var/www/braindump/scripts/
+rsync -avz etc/ $PROD_SERVER:/var/www/braindump/etc/
+scp docker-compose.yml $PROD_SERVER:/var/www/braindump
 
 # Log into Production Server, Pull and Restart Docker
-ssh circle@hydrogen.levlaz.org 'cd /var/www/braindump && docker-compose pull'
-ssh circle@hydrogen.levlaz.org 'cd /var/www/braindump && docker-compose build'
-ssh circle@hydrogen.levlaz.org 'cd /var/www/braindump && source scripts/secrets.sh && docker-compose up -d'
+ssh $PROD_SERVER 'cd /var/www/braindump && docker-compose pull'
+ssh $PROD_SERVER 'cd /var/www/braindump && docker-compose build'
+ssh $PROD_SERVER 'cd /var/www/braindump && source scripts/secrets.sh && docker-compose up -d'
